b2de89a55dbe44500fd0faa0a5ee89f98ff00cd4
Karan Gurdittaa <karan608@gmail.com>
2025-05-08 00:34:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+
+const config = {
+  businessName: "Zolo Innovations",
+  address: "C8/1, Sector 4, Eldeco Estate, One, Panipat, Simla Molana, Haryana 132103",
+  phone: "+91 9991631193",
+  email: "hello@zoloinnovations.com",
+  googleMapEmbedURL: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3474.3949345043493!2d76.9792525!3d29.4464759!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x61f4c9a365f14195%3A0xee188c1c6d2489b8!2sZolo%20Innovations%20-%20Web%20Designer%20in%20Panipat!5e0!3m2!1sen!2sin!4v1746643548349!5m2!1sen!2sin",
+  gmbProfileURL: "https://maps.app.goo.gl/rSYSYgW27jtFDuTV6",
+  socialLinks: {
+    facebook: "https://facebook.com/",
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/",
+    linkedin: "https://linkedin.com/"
+  }
+};
